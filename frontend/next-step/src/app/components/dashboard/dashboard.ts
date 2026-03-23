f8bdeda9c9369957a29aaddd545fc3d2011847b3
Angel <etti.joseangel@gmail.com>
2026-03-23 10:56:00 -0600

    Conexion en el Componente Dashboard (dashboard.ts)

--- frontend/next-step/src/app/components/dashboard/dashboard.ts
+++ frontend/next-step/src/app/components/dashboard/dashboard.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Data } from '../../services/data';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+
+  userInput: string = '';
+  chatHistory: any[] = [];
+  isTyping: boolean = false;
+
+  constructor(private dataService: Data){}
+
+  onSendMessage(){
+    if (!this.userInput.trim()) return;
+
+    // 1. Agregar mensaje del usuario a la lista
+    this.chatHistory.push({ role: 'user', text: this.userInput });
+    const tempMessage = this.userInput;
+    this.userInput = ''; //Limpiar Input
+    this.isTyping = true;
+
+    // 2. Llamamos al servicio
+    this.dataService.sendMessage(tempMessage).subscribe({
+      next: (res) => {
+        this.chatHistory.push({ role: 'assistant', text: res.response });
+        this.isTyping = false;
+      },
+      error: (err) => {
+        console.error('Error en la comunicación', err);
+        this.isTyping = false;
+      }
+    });
+  }
 
 }
