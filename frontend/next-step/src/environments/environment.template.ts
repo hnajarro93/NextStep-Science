807a901209238e1e1b986187eb43c33de66f511a
Angel <etti.joseangel@gmail.com>
2026-03-25 10:39:52 -0600

    Dashboard mejorado, chat inteligente scrolleable agregado y environment.template.ts agregado

---
+++ frontend/next-step/src/environments/environment.template.ts
@@ -0,0 +1,6 @@
+//  src/environments/environment.template.ts
+export const environment = {
+  production: false,
+  apiUrl: '', // Rellenar con la URL de Azure
+  apiKey: ''  // Rellenar si es necesario
+};
